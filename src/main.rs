102b452e5205f638b358dc65a8621ce5b5772cfb
Vivien Tang <vivien.tang@itv.com>
2024-07-26 18:19:42 +0100

    uses Clap to parse args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     file_name: String,
     path: std::path::PathBuf
 }
-fn main() {
-    let file_name = std::env::args().nth(1).expect("no file_name given");
-    let path = std::env::args().nth(2).expect("no path given");
 
-    let args = Cli {
-        file_name,
-        path: std::path::PathBuf::from(path)
-    };
+fn main() {
+    let args = Cli::parse();
 
     println!("file name: {:?}, path: {:?}", args.file_name, args.path);
 }
