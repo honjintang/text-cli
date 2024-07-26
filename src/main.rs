31566c7bdca18d81dd68112ab63ec6b42a8479db
Vivien Tang <vivien.tang@itv.com>
2024-07-26 18:27:27 +0100

    counts number of lines

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    file_name: String,
     path: std::path::PathBuf
 }
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(args.path).expect("could not read file");
 
-    println!("file name: {:?}, path: {:?}", args.file_name, args.path);
+    let num_of_lines = content.lines().count();
+
+    println!("number of lines: {:?}", num_of_lines);
 }
